06d890017d6a5b5e67e34e17ddaa2a0ac6ba8bfa
silartan <silartann@gmail.com>
2026-09-21 18:56:37 +0300

    Solution ve guncel dosyalar baglandi

--- obj/Debug/net8.0/PersonelIzinTakip.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonelIzinTakip.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonelIzinTakip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2b587aa275d687ef3eea06f107dc11bd9e1316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb04b32513c91966e3d37b0ecf9630e22474bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonelIzinTakip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonelIzinTakip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
